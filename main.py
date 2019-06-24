f00e3dd7abadab54c37023b16025b3bcd820a01e
Kristin Skelton <kskelton82@gmail.com>
2019-06-23 23:00:02 -0600

    Processing the form step

---
+++ main.py
@@ -0,0 +1,69 @@
+from flask import Flask, request
+
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+        <!-- create your form here -->
+        <form method='POST'>
+            <label>Rotate by:
+                <input type="text" name="rot" value="0"/>
+            </label>
+            <textarea name="text"></textarea>
+            <input type="submit" />          
+        </form>
+    </body>
+</html>
+"""
+
+@app.route("/")
+def index():
+    return form
+
+def is_integer(num):
+    try:
+        int(num)
+        return True
+    except ValueError:
+        return False
+
+@app.route("/", methods=['POST'])
+def encrypt():
+    rot = request.form['rot']
+    text = request.form['text']
+
+    if not is_integer(rot):
+        rot_error = 'Not a valid integer'
+        rot=''
+    else:
+        rot = int(rot)
+    
+    encrypt_string = rotate_string(text, rot)
+
+    return "<h1>" + encrypt_string + "</h1>"
+
+app.run()
